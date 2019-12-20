3cbed97d1c6355803ab33fe4aa1a7ad7acabc90e
Progroveriman <clxfxmk@qq.com>
2019-12-20 14:45:37 -0500

    Update SelfStudyTimer~~.bat
    The .bat file can activate the conda env: by adding a `call` in front of the `activate [env]`.

--- Reading_Timer/SelfStudyTimer~~.bat
+++ Reading_Timer/SelfStudyTimer~~.bat
@@ -1,8 +1,10 @@
-@echo off
+﻿@echo off
 if "%1"=="h" goto begin
 start mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
 :begin
 ::以下为正常批处理命令，不可含有pause set/p等交互命令
 cd Dropbox
+call activate py1
+::激活环境要在前面加call，参见http://cn.voidcc.com/question/p-wldusviw-bhp.html
 python SelfStudyTimer.py
 pause
